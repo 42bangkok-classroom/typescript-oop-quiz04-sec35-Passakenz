f07c0edabfb0b61e06500bd26a5c6c3cc8e88ef2
Passakenz <ken.ods.555@gmail.com>
2026-03-02 10:21:08 +0700

    eight

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,7 +5,7 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('user')
+  @Get('/user/test')
   getUser(): Array<string> {
     return this.userService.test();
   }
