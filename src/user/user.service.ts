3cc774cec465a701f6a7f97f5e4e8870090335a4
Passakenz <ken.ods.555@gmail.com>
2026-03-23 17:19:06 +0700

    p03-7

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -9,10 +9,14 @@ export class UserService {
     return [];
   }
 
-  async findAll(): Promise<Array<IUser>> {
+  async findAll(): Promise<IUser[]> {
+    try {
     const filepath = path.join(process.cwd(), 'data', 'users.json');
     const data = await fs.readFile(filepath, 'utf-8');
     const users: IUser[] = JSON.parse(data) as IUser[];
     return users;
+    } catch (error) {
+      return [] as IUser[];
   }
+}
 }
