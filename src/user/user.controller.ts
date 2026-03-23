cc3291f0acc819305381f1282b16fac5058b8852
Passakenz <ken.ods.555@gmail.com>
2026-03-23 19:34:44 +0700

    p03-19

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,7 +10,7 @@ export class UserController {
     return this.userService.test();
   }
 
-  @Get('users')
+  @Get('/users')
   async getAllUsers() {
     return await this.userService.findAll();
   }
